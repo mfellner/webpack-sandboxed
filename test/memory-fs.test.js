f9dec3e714da12aca2ceebda60d64beeb1bd7c26
Maximilian Fellner <maximilian.fellner@zalando.de>
2017-05-08 18:05:06 +0200

    test: fix tests.

--- test/memory-fs.test.js
+++ test/memory-fs.test.js
@@ -11,32 +11,37 @@ describe('memory-fs', () => {
   });
 
   it('should create a memory file-system with the given packages', async () => {
+    const root = path.resolve('../', __dirname);
     const memfs = await memoryfs.createInstance({
       packages: ['memory-fs'],
-      root: '/'
+      root
     });
 
-    let files = memfs.readdirSync('/node_modules');
+    let files = memfs.readdirSync(path.join(root, 'node_modules'));
     expect(files.length).toBeGreaterThan(1); // memory-fs has dependencies
     expect(files).toContain('memory-fs'); // memory-fs should be one of the packges
 
-    files = memfs.readdirSync('/node_modules/memory-fs/lib');
+    files = memfs.readdirSync(path.join(root, 'node_modules/memory-fs/lib'));
     expect(files).toContain('MemoryFileSystem.js');
   });
 
   it('should create a memory file-system with the given files', async () => {
+    const root = path.resolve('../', __dirname);
     const includes = [path.resolve(__dirname, '../test')];
-    const memfs = await memoryfs.createInstance({ includes, root: '/' });
+    const memfs = await memoryfs.createInstance({ includes, root });
 
-    let files = memfs.readdirSync('/');
+    let files = memfs.readdirSync(root);
     expect(files.length).toBe(1);
-    expect(files).toContain('test');
+    expect(files).toContain('includes');
 
-    files = memfs.readdirSync('/test');
+    files = memfs.readdirSync(path.join(root, 'includes'));
+    expect(files).toEqual(['test']);
+
+    files = memfs.readdirSync(path.join(root, 'includes/test'));
     expect(files.length).toBeGreaterThan(1);
     expect(files).toContain('memory-fs.test.js');
 
-    files = memfs.readdirSync('/test/__snapshots__');
+    files = memfs.readdirSync(path.join(root, 'includes/test/__snapshots__'));
     expect(files.length).toBeGreaterThan(0);
   });
 });
--- test/webpack.test.js
+++ test/webpack.test.js
@@ -8,7 +8,7 @@ describe('webpack', () => {
     const [bundle, stats] = await runner.run('var x = 41 + 1;');
 
     expect(Object.keys(bundle).length).toBe(1);
-    expect(Object.keys(bundle)[0]).toMatch(/^main-[a-z0-9]+\.min\.js$/);
+    expect(Object.keys(bundle)[0]).toMatch(/^main.[a-z0-9]+\.js$/);
     expect(typeof bundle[Object.keys(bundle)[0]]).toBe('string');
 
     expect(bundle[Object.keys(bundle)[0]]).toMatchSnapshot();
